99370a7fb3c20717bdc33bb20f0448904f582093
JianhangLin <linjianhang1010@gmail.com>
2019-10-08 09:29:42 +0800

    Initial commit

---
+++ src/main/java/work/jianhang/cfcmms/CfcmmsApplication.java
@@ -0,0 +1,13 @@
+package work.jianhang.cfcmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfcmmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfcmmsApplication.class, args);
+    }
+
+}
