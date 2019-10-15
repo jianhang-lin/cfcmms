db0b165bd41874927c0052ba24780d06a37ab852
JianhangLin <linjianhang1010@gmail.com>
2019-10-15 16:32:49 +0800

    feat(web): define Grid

---
+++ web/app/view/module/region/Grid.js
@@ -0,0 +1,33 @@
+Ext.define("app.view.module.region.Grid",{
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.modulegrid',
+    uses: [
+
+    ],
+    dockedItems: [
+
+    ],
+    columns: [
+        {
+            dataIndex: 'tf_name',
+            text: '工程项目名称',
+            width: 250
+        }, {
+            dataIndex: 'tf_budget',
+            text: '投资总额'
+        }
+    ],
+    store: new Ext.data.Store({
+        fields: ['tf_name', {
+            name: 'tf_budget',
+            type: 'float'
+        }],
+        data: [{
+            tf_name: '安居房建设工程',
+            tf_budget: 1230000
+        }, {
+            tf_name: '道路建设工程',
+            tf_budget: 453092
+        }]
+    })
+});
