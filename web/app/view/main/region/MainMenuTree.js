7e575adc08fb814a3c7df4838959312f9bf7f616
JianhangLin <linjianhang1010@gmail.com>
2019-10-15 09:37:48 +0800

    feat(web): update MainMenuTree initComponent method

--- web/app/view/main/region/MainMenuTree.js
+++ web/app/view/main/region/MainMenuTree.js
@@ -12,10 +12,68 @@ Ext.define('app.view.main.region.MainMenuTree', {
             root: {
                 text: '系统菜单',
                 leaf: false,
-                expanded: true
+                expanded: true,
+                children: []
             }
         });
-        var menus = null;
+        var menus = this.getMenus();
+        var root = this.store.getRootNode();
+        for (var i in menus) {
+            var menugroup = menus[i];
+            var menuitem = root.appendChild({
+                text: menugroup.text,
+                expanded: menugroup.expanded,
+                icon: menugroup.icon,
+                glyph: menugroup.glyph
+            });
+            for (var j in menugroup.menu) {
+                var menumodule = menugroup.menu[j];
+                var childnode = {
+                    moduleId: menumodule.text,
+                    moduleName: menumodule.module,
+                    text: menumodule.text,
+                    leaf: true
+                }
+                menuitem.appendChild(childnode);
+            }
+        }
         this.callParent(arguments);
+    },
+    getMenus: function () {
+        console.log("ButtonMainMenu getMenus...");
+        var items = [];
+        Ext.Ajax.request({
+            url: 'http://localhost:8080/system_menu',
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            async: false,
+            success: function (response, options) {
+                var menuData = Ext.decode(response.responseText).systemMenu;
+                Ext.Array.each(menuData, function (group) {
+                    var submenu = [];
+                    Ext.Array.each(group.items, function (menuitem) {
+                        submenu.push({
+                            mainmenu: 'true',
+                            moduleName: menuitem.module,
+                            text: menuitem.text,
+                            icon: menuitem.icon,
+                            glyph: menuitem.glyph,
+                            handler: 'onMainMenuClick'
+                        })
+                    });
+                    var item = {
+                        text: group.text,
+                        menu: submenu,
+                        icon: group.icon,
+                        glyph: group.glyph
+                    };
+                    items.push(item);
+                });
+                return items;
+            }
+        });
+        return items;
     }
 });
