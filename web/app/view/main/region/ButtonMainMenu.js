b198193b7c0a699d0d27ca0f401431ffec648b19
JianhangLin <linjianhang1010@gmail.com>
2019-10-15 08:57:25 +0800

    feat(web): update ButtonMainMenu

--- web/app/view/main/region/ButtonMainMenu.js
+++ web/app/view/main/region/ButtonMainMenu.js
@@ -1,8 +1,9 @@
 Ext.define("app.view.main.menu.ButtonMainMenu", {
     extend: "app.ux.ButtonTransparent",
     alias: "widget.buttonmainmenu",
+    viewModel: 'main',
     text: "菜单",
-    iconCls: "fa fa-list",
+    glyph: 0xf0c9,
     initComponent: function () {
         console.log("ButtonMainMenu initComponent");
         this.menu = this.up("app-main").getViewModel().getMenus();
