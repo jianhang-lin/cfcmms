795275810fcef07a38542e7f4559b285792c9526
JianhangLin <linjianhang1010@gmail.com>
2019-10-16 13:31:10 +0800

    feat(server): update getModule api

--- src/main/java/work/jianhang/cfcmms/controller/MainController.java
+++ src/main/java/work/jianhang/cfcmms/controller/MainController.java
@@ -90,6 +90,98 @@ public class MainController {
 
         m.setPrimaryKey("id");
         m.setNameFields("name");
+
+        List<Field> fields = new ArrayList<>();
+        Field f1 = new Field();
+        f1.setFieldId(10100010L);
+        f1.setFieldName("id");
+        f1.setTitle("序号");
+        f1.setFieldType("Integer");
+        f1.setHidden(true);
+        f1.setFieldGroup("工程基本信息");
+
+        Field f2 = new Field();
+        f2.setFieldId(10100020L);
+        f2.setFieldName("name");
+        f2.setTitle("工程项目名称");
+        f2.setFieldType("String");
+        f2.setFieldLen(50);
+        f2.setRequired(true);
+        f2.setFieldGroup("工程基本信息");
+
+        Field f3 = new Field();
+        f3.setFieldId(10100030L);
+        f3.setFieldName("code");
+        f3.setTitle("工程项目编码");
+        f3.setFieldType("String");
+        f3.setFieldLen(20);
+        f3.setRequired(true);
+        f3.setFieldGroup("工程基本信息");
+
+        Field f4 = new Field();
+        f4.setFieldId(10100040L);
+        f4.setFieldName("squaremeter");
+        f4.setTitle("建筑面积");
+        f4.setFieldType("Integer");
+        f4.setUnitText("平米");
+        f4.setFieldGroup("工程附加信息");
+        f4.setAllowSummary(true);
+
+        Field f5 = new Field();
+        f5.setFieldId(10100050L);
+        f5.setFieldName("budget");
+        f5.setTitle("投资总额");
+        f5.setFieldType("Double");
+        f5.setMoney(true);
+        f5.setFieldGroup("工程附加信息");
+        f5.setAllowSummary(true);
+
+        Field f6 = new Field();
+        f6.setFieldId(10100060L);
+        f6.setFieldName("rjl");
+        f6.setTitle("容积率");
+        f6.setFieldType("Percent");
+        f6.setFieldGroup("工程附加信息");
+
+        Field f7 = new Field();
+        f7.setFieldId(10100070L);
+        f7.setFieldName("startDate");
+        f7.setTitle("计划开工时间");
+        f7.setFieldType("Date");
+        f7.setFieldGroup("工程附加信息");
+
+        Field f8 = new Field();
+        f8.setFieldId(10100080L);
+        f8.setFieldName("endDate");
+        f8.setTitle("计划竣工时间");
+        f8.setFieldType("Date");
+        f8.setFieldGroup("工程附加信息");
+
+        Field f9 = new Field();
+        f9.setFieldId(10100090L);
+        f9.setFieldName("isValid");
+        f9.setTitle("是否通过验收");
+        f9.setFieldType("Boolean");
+        f9.setFieldGroup("工程附加信息");
+
+        Field f10 = new Field();
+        f10.setFieldId(10100100L);
+        f10.setFieldName("m3");
+        f10.setTitle("工程方量");
+        f10.setFieldType("Double");
+        f10.setFieldGroup("工程附加信息");
+
+        fields.add(f1);
+        fields.add(f2);
+        fields.add(f3);
+        fields.add(f4);
+        fields.add(f5);
+        fields.add(f6);
+        fields.add(f7);
+        fields.add(f8);
+        fields.add(f9);
+        fields.add(f10);
+        m.setFields(fields);
         module.add(m);
         map.put("module", module);
         return map;
@@ -163,6 +255,22 @@ class Module {
 
     private String primaryKey;
     private String nameFields;
-    private String fields;
+    private List<Field> fields;
     private String gridSchemes;
+}
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Field {
+    private Long fieldId;
+    private String fieldName;
+    private String title;
+    private String fieldType;
+    private boolean money;
+    private String unitText;
+    private int fieldLen;
+    private boolean isRequired;
+    private boolean hidden;
+    private String fieldGroup;
+    private boolean allowSummary;
 }
